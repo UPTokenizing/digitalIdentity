cb3a097f2aab9f3012acdd69f007212540520ffe
Juan Carlos López Pimentel <jclopezpimentel@users.noreply.github.com>
2024-08-20 14:40:32 -0600

    Smart contract GenesisIdentity creation
    GenesisIdentity was created

---
+++ smartContracts/GenesisIdentity.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: jclopezpimentel
+pragma solidity 0.8.26;
+
+contract GenesisIdentity{  
+
+    //errors
+        string constant INCORRECT_GOVERNMENT_USER = "V0001";
+    //attributes
+    string public name; 
+    string public fLastName; 
+    string public mLastName; 
+      bool public gender; //true will be man and false woman
+    uint16 public day; 
+    uint16 public month; 
+    uint16 public year;
+    string public state;
+    string public municipality;    
+      uint public dateCreation=0; // it contains the date the contract was created
+      uint public dateLastUpdate=0;
+   address public tokenFather;
+   address public tokenMother;
+   address public tokenDigIdentity;
+   address public government;
+
+  constructor(string memory _name, string memory _fLastName, string memory _mLastName, bool _gender, 
+              uint16 _day, uint16 _month, uint16 _year, string memory _state, string memory _municipality, address tFather, address tMother) {
+    name = _name; 
+    fLastName = _fLastName; 
+    mLastName = _mLastName; 
+    gender = _gender; //true will be man and false woman
+    day = _day; 
+    month = _month; 
+    year = _year;
+    state =_state;
+    municipality = _municipality;
+    dateCreation = block.timestamp;
+    dateLastUpdate = dateCreation;
+    tokenFather=(tFather==address(0))?address(0):tFather;
+    tokenMother=(tMother==address(0))?address(0):tMother;
+    tokenDigIdentity=address(0);
+    government = msg.sender;
+  }
+
+    function setFatherAddress(address fAddress) public {
+        require(msg.sender==government,INCORRECT_GOVERNMENT_USER);
+        tokenFather = fAddress;
+        dateLastUpdate = block.timestamp;
+    }
+
+    function setMotherAddress(address mAddress) public {
+        require(msg.sender==government,INCORRECT_GOVERNMENT_USER);
+        tokenMother = mAddress;
+        dateLastUpdate = block.timestamp;
+    }
+
+    function setDigitalIdentityAddress(address digIdentity) public {
+        require(msg.sender==government,INCORRECT_GOVERNMENT_USER);
+        tokenDigIdentity = digIdentity;
+        dateLastUpdate = block.timestamp;
+    }
+}
