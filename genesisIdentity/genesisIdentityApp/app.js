edf2af86177af3571a322b8c79301c2cc196dad0
Juan Carlos López Pimentel <jclopezpimentel@users.noreply.github.com>
2024-10-11 13:29:11 -0600

    SetAddress service was added
    With this service the user can set three different methods toward the smart contract. The methods are: setFatherAddress, setMotherAddress and setDigitalIdentityAddress

--- genesisIdentity/genesisIdentityApp/app.js
+++ genesisIdentity/genesisIdentityApp/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var createRouter = require('./routes/create');
 var consult = require('./routes/consult');
+var setAddress = require('./routes/setAddress');
 var proof = require('./routes/proof');
 
 var app = express();
@@ -33,6 +34,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/create', createRouter);
 app.use('/consult', consult);
+app.use('/setAddress', setAddress);
 app.use('/proof', proof);
 
 // catch 404 and forward to error handler
