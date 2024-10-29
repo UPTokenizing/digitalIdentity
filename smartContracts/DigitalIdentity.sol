0e5c8862f860b106f7db52e27afe40d2cd806c3c
Juan Carlos López Pimentel <jclopezpimentel@users.noreply.github.com>
2024-10-29 12:08:45 -0600

    Smart contracts updates
    Smart contracts updates

--- smartContracts/DigitalIdentity.sol
+++ smartContracts/DigitalIdentity.sol
@@ -1,30 +1,31 @@
 // SPDX-License-Identifier: jclopezpimentel
 pragma solidity 0.8.19;
 
-interface ContractFrom {
-        function owner() external view returns (address);
-        function nameToken() external view returns (string memory); 
-}    
+//importing the interface
+import "./OwnerInterface.sol";
 
-contract DigitalIdentity{  
+contract DigitalIdentity is OwnerInterface{  
 
     //errors
         string constant INCORRECT_GOVERNMENT_USER = "V0001";
         string constant INCORRECT_OWNER_USER = "V0002";
         string constant INCORRECT_OWNER_OF_CONTRACTADDRESS = "V0003";
         string constant INCORRECT_TOKEN_NAME = "V0004";
+        string constant TOKEN_ALREADY_EXIST = "V0005"; 
+         
 
     //attributes
-   address public owner;
-   address public government;
+        address public owner;
+            string public nameToken="DigitalIdentity";
+        address public government;
 
-   struct LinkedToken{
-        address tokenAdd;
-        string nameToken; 
-   }
-
-    mapping(address => LinkedToken) private linkedTokens;
-
+        struct LinkedToken{
+                address tokenAdd;
+                string nameToken;
+                bool exists; // Boolean flag to track whether a user exists 
+        }
+        mapping(address => LinkedToken) private linkedTokens;
+        address[] public addressesTokens;
     constructor(address _owner) {    
         owner = _owner;
         government = msg.sender;
@@ -32,15 +33,20 @@ contract DigitalIdentity{
 
     function linkToken(address contractAdd, string memory _nameToken) public {
         require(msg.sender==owner,INCORRECT_OWNER_USER);
-        ContractFrom contractFrom = ContractFrom(contractAdd);
+        OwnerInterface contractFrom = OwnerInterface(contractAdd);
         require(msg.sender==contractFrom.owner(),INCORRECT_OWNER_OF_CONTRACTADDRESS);
         require(keccak256(bytes(_nameToken)) == keccak256(bytes(contractFrom.nameToken())),INCORRECT_TOKEN_NAME);
-        linkedTokens[contractAdd] = LinkedToken(contractAdd,_nameToken);
+        require(!linkedTokens[contractAdd].exists,TOKEN_ALREADY_EXIST);
+        linkedTokens[contractAdd] = LinkedToken(contractAdd,_nameToken,true);
+        addressesTokens.push(contractAdd);
     }
 
-        // Retrieve a user's details
     function getNameToken(address _tokenAdd) public view returns (string memory) {
         LinkedToken memory lToken = linkedTokens[_tokenAdd];
         return (lToken.nameToken);
     }
-}
+
+    function numberOfLinkedTokens() public view returns (uint) {        
+        return (addressesTokens.length);
+    }
+ }
